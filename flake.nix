9e97b7d012d6c329ec7bb0cda20849c7e418809a
NotAShelf <raf@notashelf.dev>
2024-10-05 23:41:11 +0300

    pedantic nix expressions

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    lib = nixpkgs.lib;
   in {
     packages."${system}" = let
       inherit (pkgs.linuxPackages_latest) kernel;
@@ -15,80 +16,81 @@
       inherit kernel kmod;
     };
 
-    nixosConfigurations."gamma" = nixpkgs.lib.nixosSystem {
-      modules = [
-        ({
-          modulesPath,
-          config,
-          lib,
-          ...
-        }: {
-          imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-          networking.hostName = "gamma";
+    nixosConfigurations."gamma" = let
+      inherit (lib) nixosSystem;
+      inherit (lib.modules) mkDefault;
+    in
+      nixosSystem {
+        system = null;
+        modules = [
+          ({modulesPath, ...}: {
+            imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+            networking.hostName = "gamma";
 
-          boot = {
-            growPartition = true;
-            kernelParams = ["console=ttyAMA0,115200n8" "console=tty0"];
-            consoleLogLevel = lib.mkDefault 7; # ground control to kernel
-          };
+            boot = {
+              growPartition = false;
+              kernelParams = ["console=ttyAMA0,115200n8" "console=tty0"];
+              consoleLogLevel = mkDefault 7; # ground control to kernel
+            };
 
-          # Empty password
-          users.extraUsers.root.initialHashedPassword = "";
+            # Empty password
+            # root can login without a password
+            users.extraUsers.root.initialHashedPassword = "";
 
-          # Nixpkgs options
-          nixpkgs.pkgs = pkgs; # discard everything else, follow flake `pkgs`
+            # Nixpkgs options
+            nixpkgs.pkgs = pkgs; # discard everything else, follow flake `pkgs`
 
-          # Packages
-          environment.systemPackages = [pkgs.microfetch];
+            # Packages
+            environment.systemPackages = [pkgs.microfetch];
 
-          # Bootable
-          fileSystems."/boot" = {
-            device = "/dev/vda1";
-            fsType = "vfat";
-          };
+            # Bootable
+            fileSystems."/boot" = {
+              device = "/dev/vda1";
+              fsType = "vfat";
+            };
 
-          fileSystems."/" = {
-            device = "/dev/vda2";
-            fsType = "ext4";
-          };
+            fileSystems."/" = {
+              device = "/dev/vda2";
+              fsType = "ext4";
+            };
 
-          boot.loader.grub = {
-            efiSupport = true;
-            efiInstallAsRemovable = true;
-            device = "nodev";
-          };
+            boot.loader.grub = {
+              efiSupport = true;
+              efiInstallAsRemovable = true;
+              device = "nodev";
+            };
 
-          # Kernel fun
-          boot = {
-            # Use kernel package defined in flake.nix
-            kernelPackages = pkgs.linuxPackagesFor self.packages.${system}.kernel; # exposed kernel
+            # Kernel fun
+            boot = {
+              # Use kernel package defined in flake.nix
+              kernelPackages = pkgs.linuxPackagesFor self.packages.${system}.kernel; # exposed kernel
 
-            # Get test module from flake outputs
-            extraModulePackages = [self.packages.${system}.kmod];
+              # Get test module from flake outputs
+              extraModulePackages = [self.packages.${system}.kmod];
 
-            # Load module from package.
-            # Alternatively, `$ modprobe test` would work too
-            kernelModules = ["test"];
-          };
+              # Load module from package.
+              # Alternatively, `$ modprobe test` would work too
+              kernelModules = ["test"];
+            };
 
-          # Make it smaller
-          documentation = {
-            doc.enable = false;
-            man.enable = false;
-            nixos.enable = false;
-            info.enable = false;
-          };
+            # Make it smaller
+            documentation = {
+              doc.enable = false;
+              man.enable = false;
+              nixos.enable = false;
+              info.enable = false;
+            };
 
-          # Get out.
-          programs = {
-            bash.completion.enable = false;
-            command-not-found.enable = false;
-          };
+            # Get out.
+            programs = {
+              bash.completion.enable = false;
+              command-not-found.enable = false;
+            };
 
-          # Shut up.
-          system.stateVersion = "24.11";
-        })
-      ];
-    };
+            # Shut up.
+            system.stateVersion = "24.11";
+          })
+        ];
+      };
   };
 }
